7758abbec744cfda4e8876e3ffb2d95242cf1cad
ndpinguil <116099207+ndpinguil@users.noreply.github.com>
2023-11-27 00:09:35 -0500

    first commit

---
+++ src/main/kotlin/com/momentodos/proyectoclases/ProyectoclasesApplication.kt
@@ -0,0 +1,11 @@
+package com.momentodos.proyectoclases
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProyectoclasesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProyectoclasesApplication>(*args)
+}
---
+++ src/test/kotlin/com/momentodos/proyectoclases/ProyectoclasesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.momentodos.proyectoclases
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoclasesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
